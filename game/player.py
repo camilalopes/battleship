e88a32cafb7d40cf0ca803a782e09346bdb6adf8
camilalopes <kmilalopes10@gmail.com>
2019-03-17 18:40:10 -0300

    Revert "Refactors game.py"
    This reverts commit 7bba00d6d331fd2fd25aee57d2f741a0d89a850d.
    
    revert last commit

--- game/player.py
+++ game/player.py
@@ -12,11 +12,11 @@ class Player(ABC):
         self.board = Board()
 
     #@abstractmethod
-    def put_ships(self):
+    def put_ships(self, board):
         pass
 
     #@abstractmethod
-    def try_hit(self):
+    def try_hit(self, board):
         pass
 
     def get_name(self):
@@ -26,43 +26,42 @@ class Computer(Player):
     def __init__(self, name):
         super().__init__(name)
 
-    def try_hit(self):
+    def try_hit(self, board):
         valid = False
         while(valid == False):
-            x = rd.randint(0, self.board.get_rows()-1)
-            y = rd.randint(0, self.board.get_cols()-1)
-            valid = self.board.check_coordinate(x, y)
+            x = rd.randint(0, board.get_rows()-1)
+            y = rd.randint(0, board.get_cols()-1)
+            valid = board.check_coordinate(x, y)
             if valid:
-                valid = self.board.is_valid_to_shoot(x, y)
+                valid = board.is_valid_to_shoot(x, y)
 
-    def put_ships(self):
-        for ship in self.board.get_ships():
+    def put_ships(self, board):
+        for ship in board.get_ships():
             valid = False
             while(valid == False):       
-                x = rd.randint(0, self.board.get_rows()-1)
-                y = rd.randint(0, self.board.get_cols()-1)
+                x = rd.randint(0, board.get_rows()-1)
+                y = rd.randint(0, board.get_cols()-1)
                 direction = 'v' if rd.randint(0, 2) == 0 else 'h'
-                valid = self.board.check_coordinate(x, y)
+                valid = board.check_coordinate(x, y)
                 if valid:
-                    valid, self.board = self.board.put_ship(ship, x, y, direction)
-        return self.board
+                    valid = board.put_ship(ship, x, y, direction)
 
 class Human(Player):
     def __init__(self, name):
         super().__init__(name)
 
-    def try_hit(self):
+    def try_hit(self, board):
         valid = False
         while(valid == False):
             x = int(input('Coordenada X: '))
             y = int(input('Coordenada Y: '))
-            valid = self.board.check_coordinate(x, y)
+            valid = board.check_coordinate(x, y)
             if valid:
-                valid = self.board.is_valid_to_shoot(x, y)
+                valid = board.is_valid_to_shoot(x, y)
 
 
-    def put_ships(self):
-        for ship in self.board.get_ships():
+    def put_ships(self, board):
+        for ship in board.get_ships():
             valid = False
             while(valid == False):
                 print('Posicione o seu ', ship['ship'])
@@ -70,8 +69,7 @@ class Human(Player):
                 y = int(input('Coordenada inicial Y: '))
                 direction = input('Orientação (v / h): ')
 
-                valid = self.board.check_coordinate(x, y)
+                valid = board.check_coordinate(x, y)
                 if valid:
-                    valid, self.board = self.board.put_ship(ship, x, y, direction)
-            self.board.display_board(self.get_name())
-        return self.board
+                    valid = board.put_ship(ship, x, y, direction)
+            board.display_board(self.get_name())
