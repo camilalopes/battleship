3ceb27c87334f705124bf1aa66c9cba18901c43c
Giovani Moutinho <giovanim@ymail.com>
2019-03-17 14:13:57 -0300

    Updates game.py

--- game/game.py
+++ game/game.py
@@ -1,28 +1,43 @@
-from . import player
-from . import board
+from player import Human, Computer
+from board import Board
 
 class Game(object):
+	players = []
+	boards = []
+	player_turn = 0
 
 	def __init__(self):
-		player_turn = 0
-		players = []
-		players.append(Computer())
-		players.append(Human())
+		self.players.append(Computer("PC"))
+		self.players.append(Human("Jogador 1"))
 		
-		boards = []
-		boards.append(Board())
-		boards.append(Board())
-		
-		main()
+		self.boards.append(Board())
+		self.boards.append(Board())
+
+		self.main()
 		
 	def change_player(self):
 		self.player_turn = abs(self.player_turn - 1)
+
+	def have_winner(self):
+		if self.boards[0].check_game_over():
+			print(self.players[1].get_name() + " venceu a partida!")
+		else:
+			print(self.players[0].get_name() + " venceu a partida!")
 		
 	def main(self):
-		for _ in players:
-			players[player_turn].put_ships(boards[player_turn])
-			change_player()
+		for _ in self.players:
+			self.players[self.player_turn].put_ships(self.boards[self.player_turn])
+			self.change_player()
+			print("A\n", self.boards[self.player_turn].board)
 		
-		while have_winner == False:
-			players[player_turn].try_hit(boards[player_turn])
-			change_player()
+		while self.have_winner == False:
+			player_name = self.players[self.player_turn].get_name()
+			if player_name == "Jogador 1":
+				self.boards[self.player_turn].display_board(player_name)
+				self.boards[abs(self.player_turn-1)].display_enemy_board(player_name)
+
+			self.players[self.player_turn].try_hit(self.boards[self.player_turn])
+			self.change_player()	
+
+if __name__=="__main__":
+	Game()
