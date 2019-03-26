c0e15598ba7f7f130ac47cbc9cec0d147bef98ed
Giovani Moutinho <giovanim@ymail.com>
2019-03-25 23:20:29 -0300

    Makes more PEP8 adjustments

--- game/game.py
+++ game/game.py
@@ -24,59 +24,59 @@ class Game(object):
         self.player_turn = abs(self.player_turn - 1)
 
     def has_won(self, player, enemy):
-	if(self.boards[enemy].check_game_over()):
-	    self.cls()
-	    print(self.players[player].get_name() + " venceu a partida!")
-
+        if(self.boards[enemy].check_game_over()):
+            self.cls()
+            print(self.players[player].get_name() + " venceu a partida!")
+            
             print("\n Tabuleiro do(a) " + self.players[player].get_name() + ":\n")
             self.boards[self.player_turn].display_board()
-
-	    print("\n ----------------------------------------------------------------------------------------------------------------- \n")
-
-	    print("\n Tabuleiro do(a) " + self.players[enemy].get_name() + ":\n")
-			self.boards[enemy].display_board()
-
-	    return True
-	else:
-	    return False
+            
+            print("\n ----------------------------------------------------------------------------------------------------------------- \n")
+            
+            print("\n Tabuleiro do(a) " + self.players[enemy].get_name() + ":\n")
+            self.boards[enemy].display_board()
+            
+            return True
+        else:
+            return False
 
     def cls(self):
-	if os.name != "posix":
+        if os.name != "posix":
             os.system("cls")
-	else:
-	    os.system("clear")
+        else:
+            os.system("clear")
 
-	def main(self):
-		''' Players put their ships '''
-		for _ in self.players:
-			self.players[self.player_turn].put_ships(self.boards[self.player_turn])
-			time.sleep(1)
-			self.cls()
-			self.change_player()
+    def main(self):
+        ''' Players put their ships '''
+        for _ in self.players:
+            self.players[self.player_turn].put_ships(self.boards[self.player_turn])
+            time.sleep(1)
+            self.cls()
+            self.change_player()
 
-		''' The game begins and not finish until a player wins the game '''
-		while True:
-			enemy = abs(self.player_turn-1)
+        ''' The game begins and not finish until a player wins the game '''
+        while True:
+            enemy = abs(self.player_turn-1)
+            self.cls()
 
-			self.cls()
-			print("\n ==================================================== Vez do(a) " + self.players[self.player_turn].get_name() + " ====================================================  ")
-			print("\t. = Desconhecido | o = Tiro na Água | X = Navio Atingido | + = Navio Intacto")
+            print("\n ==================================================== Vez do(a) " + self.players[self.player_turn].get_name() + " ====================================================  ")
+            print("\t. = Desconhecido | o = Tiro na Água | X = Navio Atingido | + = Navio Intacto")
 
-			print("\n Seu Tabuleiro ("+self.players[self.player_turn].get_name()+"):\n")
-			self.boards[self.player_turn].display_board()
+            print("\n Seu Tabuleiro ("+self.players[self.player_turn].get_name()+"):\n")
+            self.boards[self.player_turn].display_board()
 
-			print("\n ----------------------------------------------------------------------------------------------------------------- \n")
+            print("\n ----------------------------------------------------------------------------------------------------------------- \n")
 
-			print("\n Tabuleiro do Inimigo ("+self.players[enemy].get_name()+"):\n")
-			self.boards[enemy].display_enemy_board()
-			self.players[self.player_turn].try_hit(self.boards[enemy])
+            print("\n Tabuleiro do Inimigo ("+self.players[enemy].get_name()+"):\n")
+            self.boards[enemy].display_enemy_board()
+            self.players[self.player_turn].try_hit(self.boards[enemy])
 
-			if(self.has_won(self.player_turn, enemy)):
-                            open("results.txt", "a").write(self.players[self.player_turn].get_name() + "\n")
-			    break
-			time.sleep(5)
-			
-			self.change_player()
+            if(self.has_won(self.player_turn, enemy)):
+                open("results.txt", "a").write(self.players[self.player_turn].get_name() + "\n")
+                break
+            time.sleep(5)
+            self.change_player()
+
 
 if __name__ == "__main__":
-	Game()
+    Game()
